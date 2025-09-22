25b10613c694840d52b66fa1a13076cb3b1d2fbd
n4ver <nicholaslkchng@gmail.com>
2025-09-22 17:52:33 +0800

    Update Photos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import './Home.css';
 import '../socialmedia.css'
-import image3 from '../assets/deeevelopers.jpg'
-import image1 from '../assets/github_workshop.jpg'
-import image2 from '../assets/github_workshop_2.jpg'
+import deevelopers_photo from '../assets/deeevelopers.jpg'
+//import image1 from '../assets/github_workshop.jpg'
+//import image2 from '../assets/github_workshop_2.jpg'
+import singtel_visit from '../assets/singtel_visit.jpg'
+import google_hackathon from '../assets/google_hackathon.jpg'
 
 /*
 Club Logo, Mission Tagline
@@ -16,8 +18,13 @@ function Home() {
     return (
         <div>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            <h1 style={{textAlign: "center"}}>Home of the dEEEvelopers</h1>
+            
             <div className="homeContainer">
+                <section className="connect" style={{padding: "1rem", height: "auto", paddingBottom: "1rem"}}>
+                    <h1 style={{fontFamily: "monospace"}}>dEEEvelopers.</h1>
+                    <h2 style={{textAlign: "center"}}>Where Coders Become Creators</h2>
+
+                </section>
                 <section className="card">
                     <div className="missionText">
                         <h3>Software Enthusiasts Within EEE</h3>
@@ -26,34 +33,37 @@ function Home() {
                         </p>
                         <h3>Our Mission</h3>
                         <p>To inspire, educate, and empower EEE/IEM students for software development</p>
+                        <h3>2025's Focus</h3>
+                        <p>Our core focus for 2025 is on <b>Full Stack Development</b></p>
                     </div>
                 </section>
                 <section className="image">
-                    <img src={image3}></img>
+                    <img src={deevelopers_photo}></img>
                 </section>
 
                 <section className="image">
-                    <img src={image2}></img>
+                    <img src={singtel_visit}></img>
                 </section>
-                <section className="card">
-                    <h2>What do we do?</h2>
-                    <div>
-                        <h3>Special Projects for NTU students</h3>
-                        <h3>Host Workshops & Events</h3>
-                        <h3>Support and nurture peer learning</h3>
-                    </div>
-                </section>
-
                 <section className="card">
                     <h2>Why join us?</h2>
                     <div>
-                        <h3>Enhance your Technical Skills</h3>
-                        <h3>Foster Innovative Projects</h3>
+                        <h3>Fostering Innovative & Challenging Projects</h3>
+                        <h3>Hosting Exciting Workshops & Events</h3>
+                        <h3>Supporting and nurturing peer learning</h3>
                         <h3>Connect with Industry Professionals</h3>
                     </div>
                 </section>
+
+                <section className="card">
+                    <h2>Highlights</h2>
+                    <div>
+                        <h3>Singtel Visit</h3>
+                        <h3>Google Hackathon</h3>
+                        <h3>Fullstack Workshops</h3>
+                    </div>
+                </section>
                 <section className="image">
-                    <img src={image1}></img>
+                    <img src={google_hackathon}></img>
                 </section>
 
                 <section className="connect" style={{height: '20vh'}}>
